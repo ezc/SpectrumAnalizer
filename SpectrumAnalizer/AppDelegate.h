09e57cbd924e73db989bf2c3430339d86c7a11eb
Evgeniy Kirpichenko <kirpichenko@Evgeniys-Mac-mini.local>
2013-01-04 13:44:48 +0200

    Project prototype added

---
+++ SpectrumAnalizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpectrumAnalizer
+//
+//  Created by Evgeniy Kirpichenko on 1/4/13.
+//  Copyright (c) 2013 Evgeniy Kirpichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
